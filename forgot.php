523b384692e3619111645f9fdf28e5e3dab3ae3e
Paresh <pareshnathnandi1234@gmail.com>
2024-06-01 07:00:13 +0530

    signin,login,forgot page added, js added for signup and lopgin

--- forgot.php
+++ forgot.php
@@ -17,7 +17,7 @@
 <body>
     <header>
         <div class="navbar">
-            <div class="logo"><a href="#" class="nav__logo">PURULIA DOCTOR</a></div>
+            <div class="logo"><a href="index.php" class="nav__logo">PURULIA DOCTOR</a></div>
             <ul class="links">
                 <li><a href="#" class="nav__link">Home</a></li>
                 <li><a href="#" class="nav__link">Services</a></li>
@@ -25,15 +25,42 @@
                 <li><a href="#" class="nav__link">Clinics</a></li>
                 <li><a href="#" class="nav__link">About</a></li>
             </ul>
-            <a href="signup.php" class="action_btn"><i class="ri-user-line" id="login"></i></a>
+            <div class="a-group">
+               <a href="signup.php" class="a-login" >Sign Up</a>
+               <a href="login.php" class="a-login" >Log In</a>
+            </div>
         </div>
     </header>
 
+     <!--==================== LOGIN ====================-->
+     <div class="login" id="login">
+         <form action="" class="login__form">
+            <h2 class="login__title">Log In</h2>
+            
+            <div class="login__group">
+               <!-- <div>
+                  <label for="email" class="login__label">Email</label>
+                  <input type="email" placeholder="Write your email" id="email" class="login__input">
+               </div>
+               
+               <div>
+                  <label for="password" class="login__label">Password</label>
+                  <input type="password" placeholder="Enter your password" id="password" class="login__input">
+               </div> -->
+
+                        Building this letter
+            </div>
+
+            <div>
+   
+               <button type="submit" class="login__button">Log In</button>
+            </div>
+         </form>
+
     <main class="main">
         <!-- <img src="assets/img/bg-image.png" alt="image" class="main__bg"> -->
-        <br><br><br><br>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam laboriosam, harum temporibus praesentium quasi aliquid aliquam maxime ex aspernatur nobis odit porro exercitationem totam magnam soluta rem dolorum blanditiis ipsa?</p>
     </main>
+
 
     <!--=============== MAIN JS ===============-->
     <!-- M-413-205-520 -->
