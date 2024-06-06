ab0d7625f88fab357d4ea8022170360e8855e521
Paresh <pareshnathnandi1234@gmail.com>
2024-06-07 00:37:52 +0530

    added many things , footer

--- index.php
+++ index.php
@@ -32,16 +32,16 @@
 
 
     <!-- ======= Hero Section ======= -->
-    <section id="hero" class="d-flex align-items-center">
+    <!-- <section id="hero" class="d-flex align-items-center">
         <div class="container">
             <h1>Welcome to Purulia Doctor</h1>
             <h2>We offer appointments for various clinics, providing essential health services to the community.</h2>
             <a href="#about" class="btn-get-started scrollto">Get Started</a>
         </div>
-    </section>
+    </section> -->
 
 
-    <!-- <main>
+    <main>
         <br>
         <div class="hero"
             style="background-image: url('assets/img/hero-bg.jpg'); background-size: cover; background-position: center;">
@@ -54,8 +54,175 @@
             <h2></h2>
             <p></p>
         </div>
-    </main> -->
+    </main>
 
+
+    <!--it's your boy Sudip-->
+
+
+
+    <!-- ======= Doctors Section ======= -->
+    <section id="doctors" class="doctors">
+        <div class="container">
+
+            <div class="section-title">
+                <h2>Doctors</h2>
+                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+                    consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
+                    fugiat sit in iste officiis commodi quidem hic quas.</p>
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-6">
+                    <div class="member d-flex align-items-start">
+                        <div class="pic"><img src="assets/img/doctors/doctors-1.jpg" class="img-fluid" alt=""></div>
+                        <div class="member-info">
+                            <h4>Walter White</h4>
+                            <span>Chief Medical Officer</span>
+                            <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+                            <div class="social">
+                                <a href=""><i class="ri-twitter-fill"></i></a>
+                                <a href=""><i class="ri-facebook-fill"></i></a>
+                                <a href=""><i class="ri-instagram-fill"></i></a>
+                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 mt-4 mt-lg-0">
+                    <div class="member d-flex align-items-start">
+                        <div class="pic"><img src="assets/img/doctors/doctors-2.jpg" class="img-fluid" alt=""></div>
+                        <div class="member-info">
+                            <h4>Sarah Jhonson</h4>
+                            <span>Anesthesiologist</span>
+                            <p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
+                            <div class="social">
+                                <a href=""><i class="ri-twitter-fill"></i></a>
+                                <a href=""><i class="ri-facebook-fill"></i></a>
+                                <a href=""><i class="ri-instagram-fill"></i></a>
+                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 mt-4">
+                    <div class="member d-flex align-items-start">
+                        <div class="pic"><img src="assets/img/doctors/doctors-3.jpg" class="img-fluid" alt=""></div>
+                        <div class="member-info">
+                            <h4>William Anderson</h4>
+                            <span>Cardiology</span>
+                            <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
+                            <div class="social">
+                                <a href=""><i class="ri-twitter-fill"></i></a>
+                                <a href=""><i class="ri-facebook-fill"></i></a>
+                                <a href=""><i class="ri-instagram-fill"></i></a>
+                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 mt-4">
+                    <div class="member d-flex align-items-start">
+                        <div class="pic"><img src="assets/img/doctors/doctors-4.jpg" class="img-fluid" alt=""></div>
+                        <div class="member-info">
+                            <h4>Amanda Jepson</h4>
+                            <span>Neurosurgeon</span>
+                            <p>Dolorum tempora officiis odit laborum officiis et et accusamus</p>
+                            <div class="social">
+                                <a href=""><i class="ri-twitter-fill"></i></a>
+                                <a href=""><i class="ri-facebook-fill"></i></a>
+                                <a href=""><i class="ri-instagram-fill"></i></a>
+                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <!-- End Doctors Section -->
+
+
+
+    <br><br><br><br>
+
+
+
+
+
+    <!--footer start-->
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>Medilab</h4>
+                    <ul>
+                        <li><a href="#">A108 Adam Street</a></li>
+                        <li><a href="#">New York NY 535022</a></li>
+                        <li><a href="#">United States</a></li><br> <br>
+                        <li><a href="#"><b>Phone:</b>+155895548855</a></li>
+                        <li><a href="#"><b>Email:</b>info@example.com</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><a href="index2.php">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Terms of service</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+                <!--<div class="footer-col">
+                   <h4>Our Services</h4>
+                   <ul>
+                       <li><a href="#">watch</a></li>
+                       <li><a href="#">bag</a></li>
+                       <li><a href="#">shoes</a></li>
+                       <li><a href="#">dress</a></li>
+                   </ul>
+               </div> -->
+                <div class="footer-col">
+                    <h4>follow us</h4>
+                    <div class="social-links">
+                        <a href="https://www.instagram.com/paresh.exe"><i class="ri-facebook-circle-fill"></i>
+                        <a href="https://www.instagram.com/paresh.exe"><i class="ri-twitter-fill"></i></a>
+                        <a href="https://www.instagram.com/paresh.exe"><i class="ri-instagram-fill"></i>
+                        <a href="https://www.instagram.com/paresh.exe"><i class="ri-linkedin-fill"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i>
+    </a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+
+
+    <!-- END of it's your boy Sudip-->
     <!--=============== MAIN JS ===============-->
     <!-- M-413-205-520 -->
     <!-- 123456 -->
