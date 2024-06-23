73b981282c4e9f5158d2f8aca388475df681cbe9
Paresh <pareshnathnandi1234@gmail.com>
2024-06-23 07:21:36 +0530

    doctor-details added and the minimum css added

---
+++ doctor-details.php
@@ -0,0 +1,68 @@
+<?php
+include ('./common/header.php');
+?>
+<br>
+<br>
+<br>
+<br>
+
+<div class="content">
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <div class="doctor-widget">
+                    <div class="doctor-info-left">
+                        <div class="doctor-img">
+                            <img src="assets/img/doctors/dortor1.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="doctor-info-cont">
+                            <h4 class="doc-name">Dr.Joydeep Mandal </h4>
+                            <p class="doc-speciality">MS ORTHOPEDICS &amp; Surgeon</p>
+                            <p class="doc-department">ORTHOPEDICS</p>
+                            <div class="clinic-details">
+                                <p class="doc-location">
+                                    <i class="ri-map-pin-2-fill"></i> Deep medical, E Lake Rd, <br> near science museum
+                                    <a href="javascript:void(0);">Get Directions</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="doc-info-right">
+                        <div class="clini-infos">
+                            <ul>
+                                <li><i class="ri-thumb-up-fill"></i> 99%</li>
+                                <li><i class="ri-chat-1-fill"></i> 35 Feedback</li>
+                                <li>
+                                    <i class="ri-map-pin-2-fill"></i> Deep Medical
+                                </li>
+                                <li>
+                                    <i class="ri-money-rupee-circle-fill"></i> ₹550
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="doctor-action">
+                            <a href="javascript:void(0)" class="btn btn-white fav-btn">
+                                <i class="ri-bookmark-fill"></i>
+                            </a>
+                            <a href="chat.html" class="btn btn-white msg-btn">
+                                <i class="ri-message-3-fill"></i>
+                            </a>
+                            <a href="javascript:void(0)" class="btn btn-white call-btn" data-toggle="modal"
+                                data-target="#voice_call">
+                                <i class="ri-phone-fill"></i>
+                            </a>
+                            <a href="javascript:void(0)" class="btn btn-white call-btn" data-toggle="modal"
+                                data-target="#video_call">
+                                <i class="ri-video-chat-fill"></i>
+                            </a>
+                        </div>
+                        <div class="clinic-booking">
+                            <a class="apt-btn" href="booking.html">Book Appointment</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
