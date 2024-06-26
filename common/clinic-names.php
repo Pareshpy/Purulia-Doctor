1289edf2ffe4451a0999e532758c14eead2afb6b
Paresh <pareshnathnandi1234@gmail.com>
2024-06-26 08:19:02 +0530

    clinic page updated

---
+++ common/clinic-names.php
@@ -0,0 +1,144 @@
+<br><br><br>
+<div class="container">
+
+    <div class="section-title">
+        <a style="text-decoration:none;" href="doctor.php">
+            <h2>Clinics</h2>
+        </a>
+        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+            consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
+            fugiat sit in iste officiis commodi quidem hic quas.</p>
+<br><br><br>
+
+        <div class="container-2">
+            <div class="services">
+                <div class="member d-flex align-items-start"><a href="clinic-details.php">
+                        <div class="pic">
+                            <img  class="clinic-pic"src="assets/img/clinics/image.png" alt="Ashadeep Ultrasound and Medical Centre">
+                        </div>
+                        <div class="member-info">
+                            <a style="text-decoration: none;" href="clinic-details.php"><h4>Ashadeep Ultrasound and Medical Centre</h4></a>
+                            <br><br><br>
+                            <div class="clinic-details">
+                                <p class="doc-location" style="scale: 1.3333;">
+                                    <i class="ri-map-pin-2-fill"></i> Deep medical, E Lake Rd, <br> near science museum
+                                    <a href="javascript:void(0);">Get Directions</a>
+                                </p>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <br> <br>
+        
+
+        <div class="container-2">
+            <div class="services">
+                <div class="member d-flex align-items-start"><a href="clinic-details.php">
+                        <div class="pic">
+                            <img  class="clinic-pic"src="assets/img/clinics/image.png" alt="Ashadeep Ultrasound and Medical Centre">
+                        </div>
+                        <div class="member-info">
+                            <a style="text-decoration: none;" href="clinic-details.php"><h4>Ashadeep Ultrasound and Medical Centre</h4></a>
+                            <br><br><br>
+                            <div class="clinic-details">
+                                <p class="doc-location" style="scale: 1.3333;">
+                                    <i class="ri-map-pin-2-fill"></i> Deep medical, E Lake Rd, <br> near science museum
+                                    <a href="javascript:void(0);">Get Directions</a>
+                                </p>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <br> <br>
+
+        <div class="container-2">
+            <div class="services">
+                <div class="member d-flex align-items-start"><a href="clinic-details.php">
+                        <div class="pic">
+                            <img  class="clinic-pic"src="assets/img/clinics/image.png" alt="Ashadeep Ultrasound and Medical Centre">
+                        </div>
+                        <div class="member-info">
+                            <a style="text-decoration: none;" href="clinic-details.php"><h4>Ashadeep Ultrasound and Medical Centre</h4></a>
+                            <br><br><br>
+                            <div class="clinic-details">
+                                <p class="doc-location" style="scale: 1.3333;">
+                                    <i class="ri-map-pin-2-fill"></i> Deep medical, E Lake Rd, <br> near science museum
+                                    <a href="javascript:void(0);">Get Directions</a>
+                                </p>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <br> <br>
+
+        <div class="container-2">
+            <div class="services">
+                <div class="member d-flex align-items-start"><a href="clinic-details.php">
+                        <div class="pic">
+                            <img  class="clinic-pic"src="assets/img/clinics/image.png" alt="Ashadeep Ultrasound and Medical Centre">
+                        </div>
+                        <div class="member-info">
+                            <a style="text-decoration: none;" href="clinic-details.php"><h4>Ashadeep Ultrasound and Medical Centre</h4></a>
+                            <br><br><br>
+                            <div class="clinic-details">
+                                <p class="doc-location" style="scale: 1.3333;">
+                                    <i class="ri-map-pin-2-fill"></i> Deep medical, E Lake Rd, <br> near science museum
+                                    <a href="javascript:void(0);">Get Directions</a>
+                                </p>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <br> <br>
+
+        <div class="container-2">
+            <div class="services">
+                <div class="member d-flex align-items-start"><a href="clinic-details.php">
+                        <div class="pic">
+                            <img  class="clinic-pic"src="assets/img/clinics/image.png" alt="Ashadeep Ultrasound and Medical Centre">
+                        </div>
+                        <div class="member-info">
+                            <a style="text-decoration: none;" href="clinic-details.php"><h4>Ashadeep Ultrasound and Medical Centre</h4></a>
+                            <br><br><br>
+                            <div class="clinic-details">
+                                <p class="doc-location" style="scale: 1.3333;">
+                                    <i class="ri-map-pin-2-fill"></i> Deep medical, E Lake Rd, <br> near science museum
+                                    <a href="javascript:void(0);">Get Directions</a>
+                                </p>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <br> <br>
+
+        <div class="container-2">
+            <div class="services">
+                <div class="member d-flex align-items-start"><a href="clinic-details.php">
+                        <div class="pic">
+                            <img  class="clinic-pic"src="assets/img/clinics/image.png" alt="Ashadeep Ultrasound and Medical Centre">
+                        </div>
+                        <div class="member-info">
+                            <a style="text-decoration: none;" href="clinic-details.php"><h4>Ashadeep Ultrasound and Medical Centre</h4></a>
+                            <br><br><br>
+                            <div class="clinic-details">
+                                <p class="doc-location" style="scale: 1.3333;">
+                                    <i class="ri-map-pin-2-fill"></i> Deep medical, E Lake Rd, <br> near science museum
+                                    <a href="javascript:void(0);">Get Directions</a>
+                                </p>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <br> <br>
+
+    </div>
+</div>
+
+<br><br>
+
